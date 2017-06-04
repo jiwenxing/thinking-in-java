ac273e67fae857dbc9dfa11e0c638829c1e6006f
Jverson <blite1228@163.com>
2017-06-04 21:46:55 +0800

    chapter6

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# thinking-in-java
-《thinking in java》读书笔记
+## thinking-in-java
+时隔三年重读《thinking in java》，发现之前看过的很多知识点竟然毫无印象，总结原因无非当初浅尝辄止、不加思索、不经实践。于是本次重读坚持多动手实践，多思考总结并记录读书笔记以加深理解和记忆。
+
+代码仓库为书中部分示例代码的动手实践。
+
+[wiki](https://github.com/jiwenxing/thinking-in-java/wiki)中为相关内容的读书笔记以及一些思考和总结。
---
+++ src/chapter6/IceCream.java
@@ -0,0 +1,42 @@
+/**
+ * thinking-in-java
+ * this is test code for learning java
+ */
+package chapter6;
+
+/**
+ * @author jverson
+ * 限制了Sundae构造函数的访问权限
+ * 于是调用方不能再通过构造函数创建Sundae对象，而只能通过暴露的makeASundae方式去获取Sundae对象。
+ * 还有一个效果就是私有的构造函数限制了Sundae被继承
+ */
+public class IceCream {
+
+	public static void main(String[] args) {
+		Sundae sundae = Sundae.makeASundae();
+//		Sundae sundae2 = new Sundae();  //这里不可访问，会报错
+		
+		Sundae2.access().f();
+	}
+	
+}
+
+class Sundae{
+
+	private Sundae() {}  //构造函数限制为private
+	
+	static Sundae makeASundae(){
+		return new Sundae();
+	}
+	
+}
+
+//Sundae2使用了单例（singleton）单例模式，因为你始终只能创建一个Sundae2对象
+class Sundae2{
+	private Sundae2() {}  //构造函数限制为private
+	private static Sundae2 sundae2 = new Sundae2();
+	public static Sundae2 access() {
+		return sundae2;
+	}
+	public void f() {}
+}
